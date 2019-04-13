3641dd6c63d07a75f7f91a0a686d6674b99dc7ba
Yongjae Lee <dydcks5@naver.com>
2019-04-14 00:25:52 +0900

    Node.js Mysql Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,30 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database:conf.database
+});
+connection.connect();
 
 app.get('/api/customers', (req,res) => {
-    res.send([{
-        "id": 1,
-        "image": "https://placeimg.com/64/64/any",
-        "name": "이용재",
-        "birthday": "920521",
-        "gender": "남자",
-        "job": "대학생"
-    }, {
-        "id": 2,
-        "image": "https://placeimg.com/64/64/2",
-        "name": "유니끄",
-        "birthday": "970905",
-        "gender": "여자",
-        "job": "대학생"
-    }, {
-        "id": 3,
-        "image": "https://placeimg.com/64/64/3",
-        "name": "어스엄",
-        "birthday": "930911",
-        "gender": "남자",
-        "job": "대학생"
-    }])
-})
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+});
+    app.listen(port, () => console.log(`Listening on port ${port}`));
